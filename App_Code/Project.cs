8571e0aa3084723076ff92b3ae0bc29d34083330
Jan Chalupa <janchalupa@outlook.cz>
2017-01-04 15:41:08 +0100

    no message

---
+++ App_Code/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Project
+{
+    public string Id;
+    public string Title;
+    public string Description;
+    public string Url;
+    public string ImageDesktop;
+    public string ImageMobile;
+    public string Type;
+    public string Tags;
+    public string DevelopmentState;
+    public string SupportedPlatforms;
+    public bool LivePreviewEnabled;
+    public DateTime DatePublished;
+}
